922da6884546a2d5f0ef427292d90cbe1f742b6c
Charles Oliver <charles.oliver@casumo.com>
2018-03-03 16:04:05 +0000

    refactor(app): Tidy up app component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="c-app">
+    <header className="c-app--header">
+      <h1 className="c-app-title">Acinonyx</h1>
+    </header>
+    <p className="c-app--intro">
+      Beautiful, responsive cheat sheets.
+    </p>
+  </div>
+);
 
 export default App;
