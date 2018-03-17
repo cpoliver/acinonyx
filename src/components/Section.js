fe5bf1b1c81223e1fe71d04757e45071582b05cc
Charlie Oliver <charles.oliver@casumo.com>
2018-03-17 16:37:07 +0000

    feat(grid): Super hacky first attempt at dynamic row spans

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { isEmpty, length, map, pipe, prop, reduce, sum } from 'ramda';
 
 import { mapWithIndex } from '../helpers';
 import { Item, ItemShape } from './Item';
 
 import './styles/Section.css';
+
+const calculateSpan = ({ heading, items, subsections = [] }) => {
+  let _return;
+  if (isEmpty(subsections)) {
+    _return = items.length + 1;
+  } else {
+    const countItems = pipe(prop('items'), length);
+    const summer = pipe(map, sum)
+
+    _return = subsections.length + 1 + summer(countItems, subsections)
+  }
+
+  return _return;
+};
 
 const renderItem = (item, key) => <Item {...item} key={key} />;
 
@@ -15,9 +30,8 @@ const renderSubsection = ({ subheading, items }, key) => (
   </div>
 );
 
-const Section = ({ heading, subsections, items, span = 1 }) => (
-  <div className="c-section"
-  style={{ gridRow: `span ${span}` }}>
+const Section = ({ heading, subsections, items, span = 10 }) => (
+  <div className="c-section" style={{ gridRow: `span ${calculateSpan({ heading, items, subsections })}` }}>
     <h2 className="c-section__heading">{ heading }</h2>
     { subsections ? mapWithIndex(renderSubsection, subsections) : mapWithIndex(renderItem, items) }
   </div>
